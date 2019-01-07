22a835f22e19046901c20e80c3493696807975a5
Santosh <santosh.bluethink@gmail.com>
2019-01-07 18:59:05 +0530

    home page

--- viodyne_webapp/home/models.py
+++ viodyne_webapp/home/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeSlider(models.Model):
+	Name = models.CharField(max_length=120)
+	Image =models.ImageField(upload_to='slider/')
+
+	def __str__(self):
+		return self.Name
+
+class HomeSlider2(models.Model):
+	Name  = models.CharField(max_length=120)
+	Name2 = models.CharField(max_length=120)
+	Text  = models.CharField(max_length=300)
+	Image = models.ImageField(upload_to='slider2/')
+	
+	def __str__(self):
+		return self.Name					
+
+class Homeslide3(models.Model):
+	Name  = models.CharField(max_length=120)
+	Image = models.ImageField(upload_to='slider3/')
+
+	def __str__(self):
+		return self.Name
+
+class Reporter(models.Model):
+	full_name = models.CharField(max_length=120)
+
+	def __str__(self):
+		return self.full_name
+
+class Artical(models.Model):
+	pub_date = models.DateField()
+	headline = models.CharField(max_length=300)
+	content = models.TextField()
+	reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.content
+						
+	
+
+
